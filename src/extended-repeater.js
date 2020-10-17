12a381caa57f42d8cd4a003cfff8834263b48587
KlimukVI <KlimukVI@rambler.ru>
2020-10-17 15:31:27 +0300

    feat: add problem solver

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addStr, pickStr;
+  if (options.repeatTimes === undefined) { options.repeatTimes = 1; }
+  if (options.additionRepeatTimes === undefined) { options.additionRepeatTimes = 1; }
+  if (!options.separator) {options.separator = '+'}
+  if (!options.additionSeparator) {options.additionSeparator = '|'}
+  if (options.addition === undefined) { addStr = ''; }
+  else {
+    addStr = String(options.addition).concat(options.additionSeparator).repeat(options.additionRepeatTimes).slice(0,-options.additionSeparator.length);
+  }
+  pickStr = String(str).concat(addStr).concat(options.separator).repeat(options.repeatTimes).slice(0,-options.separator.length);
+  return pickStr;
 };
-  
